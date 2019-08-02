28a625f2033df27931e766ba788c5e4d285a8b55
gleisonsdm <gleison14051994@gmail.com>
2019-08-02 23:10:02 +0000

    OpenMP Extractor repository cleaned.

--- clangPlugin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -r build/
-mkdir build
-cd build/
-cmake -DLLVM_DIR=/home/souzadinizme1/workdir/llvm-8.0/llvm-build/lib/cmake/llvm/ ../
-make
-cd ../
--- clangPlugin/example/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-void foo(int *a, int *b, int n) {
-  #pragma omp parallel for
-  for (int i = 0; i < n; i++) {
-    a[i] = b[i];
-  }
-}
